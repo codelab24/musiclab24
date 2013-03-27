665114022dd06bc05c1f51bd5f4a76582241a72c
Developer <codelab24@gmail.com>
2013-03-27 10:13:46 +0300

    Basic security Added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Application\Api\DefaultBundle\ApplicationApiDefaultBundle(),
+            new Application\Api\UserBundle\ApplicationApiUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
